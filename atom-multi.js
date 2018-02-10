e8043cb86ee1b6c7e4255774a244efa6c759f370
Lucas Chen <2109121+parkroolucas@users.noreply.github.com>
2018-02-10 17:32:28 +1100

    Initial commit

---
+++ atom-multi.js
@@ -0,0 +1,28 @@
+// atom-multi ~/work/projects/ react-*
+
+const [projectDir, globPattern] = process.argv.slice(2); // remove "node" and "atom-multi.js"
+
+const { lstatSync, readdirSync } = require("fs");
+const minimatch = require("minimatch");
+const path = require("path");
+const child_process = require("child_process");
+
+const isDirectory = source => lstatSync(source).isDirectory();
+const getDirectories = source =>
+  readdirSync(source)
+    .map(name => path.join(source, name))
+    .filter(isDirectory);
+
+if (!isDirectory(projectDir)) throw new Error(`${projectDir} is not a directory!`);
+
+const toOpen = [];
+
+getDirectories(projectDir).forEach(dirPath => {
+  const dirName = path.basename(dirPath);
+
+  if (minimatch(dirName, globPattern)) {
+    toOpen.push(dirPath);
+  }
+});
+
+child_process.execSync(`atom ${toOpen.join(" ")}`);
---
+++ remember-to-install-atom.js
@@ -0,0 +1,3 @@
+require("colors");
+
+console.log("Make sure Atom exists in your PATH!".magenta);
